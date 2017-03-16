605caa5d87d6d3e85cedb910d12b0935b3609db3
Mike Robertson <mjr127+github@case.edu>
2017-03-16 09:45:34 -0400

    add color

--- src/Icons/Loading.jsx
+++ src/Icons/Loading.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+import theme from '../themes';
 
 const Loading = ({ className }) => (
   <svg
+    className={className}
     version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
     width="24px" height="30px" viewBox="0 0 24 30" style={{ enableBackground: 'new 0 0 50 50' }}
   >
-    <rect x="0" y="0" width="4" height="10" fill="#333">
+    <rect x="0" y="0" width="4" height="10" fill={theme.palette.textColorPrimary}>
       <animateTransform
         attributeType="xml"
         attributeName="transform" type="translate"
@@ -13,7 +16,7 @@ const Loading = ({ className }) => (
         begin="0" dur="0.6s" repeatCount="indefinite"
       />
     </rect>
-    <rect x="10" y="0" width="4" height="10" fill="#333">
+    <rect x="10" y="0" width="4" height="10" fill={theme.palette.textColorPrimary}>
       <animateTransform
         attributeType="xml"
         attributeName="transform" type="translate"
@@ -21,7 +24,7 @@ const Loading = ({ className }) => (
         begin="0.2s" dur="0.6s" repeatCount="indefinite"
       />
     </rect>
-    <rect x="20" y="0" width="4" height="10" fill="#333">
+    <rect x="20" y="0" width="4" height="10" fill={theme.palette.textColorPrimary}>
       <animateTransform
         attributeType="xml"
         attributeName="transform" type="translate"
@@ -31,5 +34,9 @@ const Loading = ({ className }) => (
     </rect>
   </svg>
 );
+
+Loading.propTypes = {
+  className: PropTypes.string,
+};
 
 export default Loading;
